54d627cf918f1119686aebf30683eee5a85be502
kkramp <kkramp@gericall.nl>
2022-06-23 01:26:04 +0200

    connected with api

--- apollo-client-project/src/Pages/Home.js
+++ apollo-client-project/src/Pages/Home.js
@@ -2,10 +2,12 @@ import { ApolloClient } from '@apollo/client'
 import React from 'react'
 import {useLazyQuery } from "@apollo/client"
 import GET_WEATHER from "../GraphQL/Queries"
+import { useState } from 'react'
 
 function Home() {
+    const [citySearched, setCitySearched] = useState("")
     const [getWeather, {loading, data, error}] =  useLazyQuery(GET_WEATHER, 
-            {variables: {name: "Vancouver"}},
+            {variables: {name: citySearched}},
         )
 
     if (error) return <h1> Error found</h1>
@@ -17,8 +19,17 @@ function Home() {
         <div className="Home">
 
             <h1>Search for  weather</h1>
-            <input type="text" placeholder='Cutyname' />
+            <input type="text" placeholder='City name' onClick={(e)=>setCitySearched(e.target.value)}/>
             <button onClick={() => getWeather()}>Search</button>
+            {data &&
+            (
+                <>
+                <h1>Place:</h1>
+                <h1>Rain</h1>
+                <h1>Temp</h1>
+                </>
+            )
+            }
         </div>
     )
 }
