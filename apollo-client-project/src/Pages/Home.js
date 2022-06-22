dc97c4c727c013e0d1644328b31394382e57855f
kkramp <kkramp@gericall.nl>
2022-06-22 16:14:47 +0200

    button zoeken disable

---
+++ apollo-client-project/src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className="Home">
+
+            <h1>Search for  weather</h1>
+            <input type="text" placeholder='Cutyname' />
+            <button>Search</button>
+        </div>
+    )
+}
+
+export default Home
